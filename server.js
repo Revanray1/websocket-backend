77d6afb1c69f38b7a31e56dc819f5cfa2da6029d
Revan <revankumars332@gmail.com>
2024-02-08 17:20:09 +0530

    frontend netlify URL implemeted

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const app = express();
 const http = require("http");
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 
 const io = new Server(server, {
   cors: {
-      origin: "*",
+      origin: "https://chatandsay.netlify.app/",
       methods: ["GET", "POST"]
   }
 });
@@ -15,18 +15,7 @@ const io = new Server(server, {
 io.on("connection", (socket) => {
   console.log("A user connected", socket.id);
   socket.emit("message", [
-    { name: "carlie", text: "heiiii", image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "hello" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "Revan", text: "What are u Doing ?" ,image:"https://i.imgur.com/DY6gND0.png"},
-    { name: "mary", text: "Work ?What About You" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "Revan", text: "hahahha okay" ,image:"https://i.imgur.com/DY6gND0.png"},
-    { name: "mary", text: "mm  same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "mm  same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "mm  same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "mm jehgrghjkghjkfhiuhgfhfdjhifdhgoiuerbhgoibdiughbhgfhggerhoguhodiugbhiugdfiogbfighbqiudgfbuqhgbyrugberhby same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "mm  same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "mm  same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
-    { name: "mary", text: "mm  same 😊" ,image:"https://i.imgur.com/HYcn9xO.png"},
+    { name: "Demo", text: "say HEllo", image:"https://i.imgur.com/HYcn9xO.png"}
   ]);
 
   socket.on("message", (data) => {
